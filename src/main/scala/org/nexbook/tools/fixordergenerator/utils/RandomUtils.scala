5ac1b5626e3fa7714e853ef0659f30bc67a20e97
Bartosz Milczarek <bartosz@milczarek.it>
2014-12-20 20:34:40 +0100

    added weighted random function

--- src/main/scala/org/nexbook/tools/fixordergenerator/utils/RandomUtils.scala
+++ src/main/scala/org/nexbook/tools/fixordergenerator/utils/RandomUtils.scala
@@ -10,4 +10,11 @@ object RandomUtils {
   def random(start: Double, end: Double): Double = {
     Random.nextDouble() * (end - start) + start
   }
+
+  def weightedRandom[T](list: List[(Int, T)]): T = {
+    val weightSum = list.foldLeft(0)(_ + _._1)
+    val randomVal = RandomUtils.random(0.0, weightSum)
+    val weightedMap = list.view.zipWithIndex.map(ziped => (list.take(ziped._2+1).foldLeft(0.0)(_ + _._1), list(ziped._2)._2)).toMap
+    weightedMap.filterKeys(randomVal < _).values.head
+  }
 }
